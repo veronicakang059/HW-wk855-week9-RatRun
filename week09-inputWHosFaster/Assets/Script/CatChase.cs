faade3752a16c3cc3a1230125b550c01011f8848
Wei <wk855@nyu.edu>
2021-04-03 19:29:44 -0400

    adding unity

---
+++ week09-inputWHosFaster/Assets/Script/CatChase.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class CatChase : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Queue<int> catMove = new Queue<int>();
+    public GameObject cat;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            catMove.Enqueue(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            catMove.Enqueue(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            catMove.Enqueue(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            catMove.Enqueue(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            showChase();
+        }
+        
+    }
+
+    void showChase()
+    {
+        while (catMove.Count > 0)
+        {
+            switch (catMove.Dequeue())
+            {
+                case 0:
+                    cat.transform.Translate(0,1,0);
+                    break;
+                case 1:
+                    cat.transform.Translate(0,-1,0);
+                    break;
+                case 2:
+                    cat.transform.Translate(-1,0,0);
+                    break;
+                case 3:
+                    cat.transform.Translate(1,0,0);
+                    break;
+                default:
+                    break;
+                    
+            }
+        }
+    }
+}
---
+++ week09-inputWHosFaster/Assets/Script/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    private Queue<int> attackInput = new Queue<int>();
+    public GameObject player;
+    private bool playAttack = false;
+    public Text displaytext;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            attackInput.Enqueue(0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            attackInput.Enqueue(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            attackInput.Enqueue(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            attackInput.Enqueue(3);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playAttack = true;
+            showAttack();
+        }
+        
+    }
+
+    void showAttack()
+    {
+        while (attackInput.Count > 0)
+        {
+            switch (attackInput.Dequeue())
+            {
+                case 0:
+                    player.transform.Translate(-1,0,0);
+                    break;
+                case 1:
+                    player.transform.Translate(0,-1,0);
+                    break;
+                case 2:
+                    player.transform.Translate(1,0,0);
+                    break;
+                case 3:
+                    player.transform.Translate(0,1,0);
+                    break;
+                default:
+                    break;
+            }
+        }
+        playAttack = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        displaytext.text = "Rat lost!";
+    }
+}
